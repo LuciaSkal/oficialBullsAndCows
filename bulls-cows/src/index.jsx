92d1befe0c50a5b35360cada7c11f28e1a3ccf4f
Sabina Dědečková <sdmsi666@gmail.com>
2021-06-05 11:49:10 +0200

    komponenta-Controls

--- bulls-cows/src/index.jsx
+++ bulls-cows/src/index.jsx
@@ -9,71 +9,71 @@ import './style.css';
 const Game = () => {
   function randomUnique(set, delka) {
     if (delka > set.length) {
-        throw new Error("Cannot generate that many unique characters.");
+      throw new Error('Cannot generate that many unique characters.');
     }
     const arr = [...set];
     for (let i = 0; i < delka; i++) {
-        const r = i + Math.floor(Math.random() * (set.length - i));
-        [arr[i], arr[r]] = [arr[r], arr[i]];
+      const r = i + Math.floor(Math.random() * (set.length - i));
+      [arr[i], arr[r]] = [arr[r], arr[i]];
     }
     return arr.slice(0, delka);
-}
+  }
 
-function parse(set, delka, input) {
+  function parse(set, delka, input) {
     if (input.length !== delka) {
-        return 'length';
+      return 'length';
     }
-    const split = input.split("");
+    const split = input.split('');
     for (let i = 0; i < split.length; i++) {
-        const c = split[i];
-        if (!set.includes(c)) {
-            return 'not-in-set';
-        }
-        if (split.lastIndexOf(c) > i) {
-            return 'repeating';
-        }
+      const c = split[i];
+      if (!set.includes(c)) {
+        return 'not-in-set';
+      }
+      if (split.lastIndexOf(c) > i) {
+        return 'repeating';
+      }
     }
     return split;
-}
+  }
 
-function check(rnd, tip) {
+  function check(rnd, tip) {
     const result = {
-        bulls: 0,
-        cows: 0,
-        calf: 0
+      bulls: 0,
+      cows: 0,
+      calf: 0,
     };
     for (let i = 0; i < rnd.length; i++) {
-        if (rnd[i] === tip[i]) {
-            result.bulls++;
-        } else if (rnd.includes(tip[i])) {
-            result.cows++;
-        } else {
-            result.calf++;
-        }
+      if (rnd[i] === tip[i]) {
+        result.bulls++;
+      } else if (rnd.includes(tip[i])) {
+        result.cows++;
+      } else {
+        result.calf++;
+      }
     }
-    return result
-}
+    return result;
+  }
 
-function reportError(error) {
+  function reportError(error) {
     switch (error) {
-        case 'length':
-            alert("This number is too long or short to be valid!");
-            break;
-        case 'not-in-set':
-            alert("The guess contains an illegal character!");
-            break;
-        case 'repeating':
-            alert("The guess contains a repeating digit!");
-            break;
-        default:
-            alert(`Error: ${error}`);
+      case 'length':
+        alert('This number is too long or short to be valid!');
+        break;
+      case 'not-in-set':
+        alert('The guess contains an illegal character!');
+        break;
+      case 'repeating':
+        alert('The guess contains a repeating digit!');
+        break;
+      default:
+        alert(`Error: ${error}`);
     }
-}
+  }
 
-function game(wrapper, set, delka) {
-    const oznam = wrapper.querySelector('.oznam')
-    const tip = wrapper.querySelector('.tip')
-    const button = wrapper.querySelector('button')
+  function game(wrapper, set, delka) {
+    const oznam = wrapper.querySelector('.oznam');
+    const tip = wrapper.querySelector('.tip');
+    const button = wrapper.querySelector('button');
 
     let rnd = randomUnique(set, delka);
     let history = [];
@@ -94,12 +94,13 @@ function game(wrapper, set, delka) {
             oznam.innerHTML += `${input} : ${bulls} bulls | ${cows} cows | guesses: ${history.length} <br>`
         }
     });*/
-}
+  }
 
   //game(document, "0123456789".split(""), 4);
 
   const handleGuess = (guess) => {
-   /* const input = tip.value;
+    console.log(guess);
+    /* const input = tip.value;
     const parsed = parse(set, delka, input)
     if (typeof parsed === 'string') {
         reportError(parsed);
@@ -113,18 +114,16 @@ function game(wrapper, set, delka) {
     } else {
         oznam.innerHTML += `${input} : ${bulls} bulls | ${cows} cows | guesses: ${history.length} <br>`
     }*/
-  }
+  };
 
   return (
     <>
-    <Header />
-    <Images />
-    <History />
-    <Controls onGuess={handleGuess} />
+      <Header />
+      <Images />
+      <History />
+      <Controls onGuess={handleGuess} />
     </>
   );
-}
-  
-
+};
 
 render(<Game />, document.querySelector('#app'));
