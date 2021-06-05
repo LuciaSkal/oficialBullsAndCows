5e27f315002226945f6bdbafe0f12e380cff1093
Sabina Dědečková <sdmsi666@gmail.com>
2021-06-05 16:35:56 +0200

    index styly

--- bulls-cows/src/index.jsx
+++ bulls-cows/src/index.jsx
@@ -1,9 +1,24 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
 import { Game } from './Game';
+import './style.css';
+import imgTop from './img/img-on-top.svg';
+import imgBottom from './img/img-at-the-bottom.svg';
 
 const App = () => {
-  return <Game set={'0123456789'.split('')} delka={4} />;
+  return (
+    <div className="container">
+      <div className="top-image">
+        <img src={imgTop} alt="blok" />
+      </div>
+      <div className="content">
+        <Game set={'0123456789'.split('')} delka={4} />
+      </div>
+      <div className="bottom-image">
+        <img src={imgBottom} alt="blok" />
+      </div>
+    </div>
+  );
 };
 
 render(<App />, document.querySelector('#app'));
