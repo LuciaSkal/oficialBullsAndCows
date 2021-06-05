69e2efa8b0f938f701b86321c880e0c3ff5ffaaa
Sabina Dědečková <sdmsi666@gmail.com>
2021-06-05 17:27:10 +0200

    problem s obrazky

--- bulls-cows/src/components/Images/Images.jsx
+++ bulls-cows/src/components/Images/Images.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import imgBull from './img/svg_bull.svg';
+import imgCow from './img/svg_cow.svg';
+import './style.css';
 
 export const Images = () => {
   return (
-    <h1>Images</h1>
+    <div className="images">
+      <img src={imgBull} alt="býk" />
+      <img src={imgCow} alt="kráva" />
+    </div>
   );
 };
