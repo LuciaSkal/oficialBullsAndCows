19df45052b38d8dad8caad4f72618b10450c86b9
Sabina Dědečková <sdmsi666@gmail.com>
2021-06-06 16:16:08 +0200

    blablabla

--- bulls-cows/src/components/History/History.jsx
+++ bulls-cows/src/components/History/History.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 export const History = ({ history }) => {
   return (
-    <>
+    <div className="history-container">
       <ol>
         {history.map((item, index) => (
           <li className="guess-history" key={index}>
-            <span>{item.bulls}</span> <span className="arrow">{'<--'}</span>
-            <span className="players-guess">{item.guess}</span>{' '}
-            <span className="arrow">{'-->'}</span>
+            <span>{item.bulls}</span> <span className="arrow">{"<--"}</span>
+            <span className="players-guess">{item.guess}</span>{" "}
+            <span className="arrow">{"-->"}</span>
             <span>{item.cows}</span>
           </li>
         ))}
       </ol>
-    </>
+    </div>
   );
 };
