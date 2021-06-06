113ec2cc9cb3e55a7580cd863e6c89b72bac3f1d
Lucia Skalinova <skalinovalucia@gmail.com>
2021-06-06 13:00:37 +0200

    styly pro komponentu SelectGame

--- bulls-cows/src/components/SelectGame/SelectGame.jsx
+++ bulls-cows/src/components/SelectGame/SelectGame.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Images } from '../Images/Images';
+import "./style.css";
 
 
 export const SelectGame = () => {
   
   return (
-    <>
-      <Link to="/game4">Generate Code</Link> {" "}
-      <Link to="/game6">Generate Code</Link> {" "} 
-      <Link to="/game8">Generate Code</Link>
-    </>  
+    <div>
+      <Images/>
+      <h2>Play game with</h2>
+      <div className="select-game">
+        <Link to="/game4" className="game">4 digits</Link> {" "}
+        <Link to="/game6" className="game">6 digits</Link> {" "} 
+        <Link to="/game8" className="game">8 digits</Link>
+      </div>
+    </div>  
   );
 };
