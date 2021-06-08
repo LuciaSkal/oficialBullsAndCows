d2b7960b0b172ac3a4a95fc494b9e46f6c8499de
Sabina Dědečková <sdmsi666@gmail.com>
2021-06-08 23:07:59 +0200

    tutorial

--- src/components/Tutorial/Tutorial.jsx
+++ src/components/Tutorial/Tutorial.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import "./style.css";
+import first from "./img/tutorial_1.svg";
+import second from "./img/tutorial_2.svg";
+import third from "./img/tutorial_3.svg";
+import forth from "./img/tutorial_4.svg";
+import fifth from "./img/tutorial_5.svg";
+import home from "../Header/img/home.svg";
+import arrow from "./img/up-arrow.svg";
 
 export const Tutorial = () => {
+  const [picture, setPicture] = useState(0);
+  const pictures = [first, second, third, forth, fifth];
+
   return (
-    <div >
-     <h1>Tutorial</h1>
+    <div className="tutorial_container">
+      <h1>Tutorial</h1>
+
+      <button className="home">
+        <img className="home_icon" src={home} alt="main-menu-icon" />
+      </button>
+
+      <div className="carousel">
+        <button
+          disabled={picture === 0 ? true : false}
+          onClick={() => setPicture(picture - 1)}
+          className="carousel_previous"
+        >
+          <img id="left" src={arrow} alt="arrow" />
+        </button>
+
+        <div className="carousel_media">
+          <img
+            className="carousel_image"
+            src={pictures[picture]}
+            alt="tutorial"
+          />
+        </div>
+
+        <button
+          disabled={picture === pictures.length - 1 ? true : false}
+          onClick={() => setPicture(picture + 1)}
+          className="carousel_next"
+        >
+          <img id="right" src={arrow} alt="arrow" />
+        </button>
+      </div>
+      <a className="linkPlay" href="#">
+        Let's play!!!
+      </a>
     </div>
   );
 };
