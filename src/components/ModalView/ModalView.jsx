266301336bf72a711920cbdb5c6a6e8030f79900
Sabina Dědečková <sdmsi666@gmail.com>
2021-06-12 18:03:38 +0200

    komponenta ModalView

--- src/components/ModalView/ModalView.jsx
+++ src/components/ModalView/ModalView.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
+import React from "react";
 import { Timer } from "../Header/Timer";
 import "./style.css";
-import { useHistory } from 'react-router-dom';
-import home from '../Header/img/home.svg';
+import { useHistory } from "react-router-dom";
+import home from "../Header/img/home.svg";
+import cowcorn from "./img/cowcorn.svg";
 
 export const ModalView = ({ pocetPokusu, start, stop }) => {
   const history = useHistory();
   const handleClick = () => {
-    history.push('/')
-  } 
+    history.push("/");
+  };
 
   return (
-    <div className="styly">
-      
-     <h2>You won 🎉</h2>
-  
-      <span>Number of attempts: {pocetPokusu}</span> 
-    
-      <span>Time needed: <Timer start={start} stop={stop} /></span>
+    <div className="winner-baner">
+      <p>cowcorn says</p>
+      <h1>congrats</h1>
+
+      <img className="cowcorn" src={cowcorn} alt="" />
+
+      <span>Number of attempts: {pocetPokusu}</span>
+
+      <span>
+        Time needed: <Timer start={start} stop={stop} />
+      </span>
       <button onClick={handleClick} className="home-icon">
-      <img className="icon" src={home} alt="" />
+        <img className="icon" src={home} alt="" />
       </button>
     </div>
   );
